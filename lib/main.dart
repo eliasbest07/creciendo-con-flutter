b1ba215348a46ad710aef4a3944acb0aa5f99ef4
Hugo Grados <hdgch1106@gmail.com>
2023-06-07 16:39:41 -0500

    Conexion a firebase establecida

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'screens/screens.dart';
-void main() => runApp(const MyApp());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-@override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'AppTitle ', //Titulo
       initialRoute: 'home', // inicial rutas y screens.dart
       routes: {
-      'home': (_) => const HomeScreen(),
-      'login': (_) => const LoginScreen(),
+        'home': (_) => const HomeScreen(),
+        'login': (_) => const LoginScreen(),
       },
     );
   }
