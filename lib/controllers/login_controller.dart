32dd23b598c2e1e8fe51e291b5799bcd55fbaa7c
Carlos Peñaranda <67028929+carlostic26@users.noreply.github.com>
2023-06-09 20:24:33 -0500

    login conectado

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,3 +1,4 @@
+import 'package:creciendo_con_flutter/infrastructure/services/firebaseAuthentication.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/login.dart';
@@ -7,6 +8,9 @@ class LoginController extends StateNotifier<Login>{
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _name = TextEditingController();
+
+  final FirebaseAuthentication auth = FirebaseAuthentication();
   
   void setLogin(Login login){
     state = login;
@@ -15,14 +19,20 @@ class LoginController extends StateNotifier<Login>{
   get inputControllerEmail => _emailController;
   get inputControllerPassword => _passwordController;
 
-  void inciarSesion() { // async 
+  void inciarSesion() async { // async 
     state = Login(email: _emailController.text, password: _passwordController.text);
     print(' correo = ${state.email} | clave= ${state.password}');
+    await auth.signIn(state.email, state.password);
     // await FirebaseAuth.instance.signInWithEmailAndPassword(
     // manda a firebase <--
   }
-  void registrar(Login credenciales){
-    
+  void registrar(Login credenciales) async{
+    await auth.signUp('carlos', state.email, state.password);
+  }
+
+
+  void cerrarSesion() async{
+    await auth.signOut();
   }
   // se conecta al backend Firebase
 
