a00aa5f0f5ffd974e66746f08e283efa4a0f7fbb
Hugo Grados <hdgch1106@gmail.com>
2023-06-29 19:14:58 -0500

    Exceptions agregados a firebaseAuthentication

--- lib/domain/exceptions/exceptions.dart
+++ lib/domain/exceptions/exceptions.dart
@@ -2,10 +2,31 @@ class UserNotFound implements Exception {
   final String message;
 
   UserNotFound(this.message);
+
+  @override
+  String toString() {
+    return "UserNotFound: $message";
+  }
 }
 
 class SignUpFailed implements Exception {
   final String message;
 
   SignUpFailed(this.message);
+
+  @override
+  String toString() {
+    return "SignUpFailed: $message";
+  }
+}
+
+class UserStorageFailed implements Exception {
+  final String message;
+
+  UserStorageFailed(this.message);
+
+  @override
+  String toString() {
+    return "UserStorageFailed: $message";
+  }
 }
