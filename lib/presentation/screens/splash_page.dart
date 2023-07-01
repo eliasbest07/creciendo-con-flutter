7c60564d577ff6b4a8f70c3f0f3cae07d775b101
EliasMontilla <eliasbest634@gmail.com>
2023-07-01 07:17:27 -0500

    update Splash Page

--- lib/presentation/screens/splash_page.dart
+++ lib/presentation/screens/splash_page.dart
@@ -27,6 +27,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
