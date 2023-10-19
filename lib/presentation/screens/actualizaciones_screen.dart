baea6c54ad27f9fb0af4faafcf1a547e9f8c27ea
Carlos Peñaranda <67028929+carlostic26@users.noreply.github.com>
2023-10-18 20:35:49 -0500

    cargar lista lideres notificacion

---
+++ lib/presentation/screens/actualizaciones_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Actualizaciones extends StatelessWidget {
+  const Actualizaciones({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            title: const Text('ACTUALIZACIONES', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+            centerTitle: true,
+        ),
+        body: Column(children: [
+            const Text('Estos son los ultimos mensajes recibidos')
+        ],)
+    );
+  }
+}
